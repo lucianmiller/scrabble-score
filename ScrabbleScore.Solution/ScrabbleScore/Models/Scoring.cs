2a16922dfc89e9cbb97e51b71575e24f8d65f1d0
Lucian Miller <lucian.miller185@gmail.com>
2022-09-02 11:10:26 -0700

    Add constructor for Scoring class

--- ScrabbleScore.Solution/ScrabbleScore/Models/Scoring.cs
+++ ScrabbleScore.Solution/ScrabbleScore/Models/Scoring.cs
@@ -1,9 +1,28 @@
-using System;
+//using System;
 
 namespace ScrabbleScore.Models
 {
   public class Scoring
   {
-    
+    public string PlayerWord { get; set; }
+    public string[] ScoreOne { get; set; }
+    public string[] ScoreTwo { get; set; }
+    public string[] ScoreThree { get; set; }
+    public string[] ScoreFour { get; set; }
+    public string[] ScoreFive { get; set; }
+    public string[] ScoreEight { get; set; }
+    public string[] ScoreTen { get; set; }
+
+    public Scoring(string playerWord) 
+    {
+      ScoreOne = new string[] { "a", "e", "i", "o", "u","l", "n", "r", "s", "t" };
+      ScoreTwo = new string[] { "d", "g" };
+      ScoreThree = new string[] { "b", "c", "m", "p" };
+      ScoreFour = new string[] { "f", "h", "v", "w", "y" };
+      ScoreFive = new string[] { "k" };
+      ScoreEight = new string[] { "j", "x" };
+      ScoreTen = new string[] { "q", "z" };
+      PlayerWord = playerWord;
+    }
   }
 }
